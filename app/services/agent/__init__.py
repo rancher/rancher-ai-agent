563963f52bf43e03519a02b44f19f4be453e8df5
raul <raul.cabello@suse.com>
2026-04-23 15:11:28 +0200

    deepagent

--- app/services/agent/__init__.py
+++ app/services/agent/__init__.py
@@ -1,4 +1,4 @@
 from .child import ChildAgentBuilder
 from .root import RootAgentBuilder
-from .parent import ParentAgentBuilder, ChildAgent
+from .parent import create_parent_agent
 from .base import AgentState

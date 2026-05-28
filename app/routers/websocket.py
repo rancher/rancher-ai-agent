2ebe61fef0700f4fa34a1869ffecb6394de6e8fa
Raul Cabello <raulcabm@gmail.com>
2026-05-28 13:22:11 +0200

    increase recursion limit

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -351,7 +351,7 @@ def _build_config(base_config: dict, request_id: str, ws_request: WebSocketReque
     config = {
         **base_config,
         "configurable": {**base_config["configurable"]},
-        "recursion_limit": 25,
+        "recursion_limit": 50, # Prevent infinite recursion in case of misbehaving agents
     }
 
     config["configurable"]["request_id"] = request_id

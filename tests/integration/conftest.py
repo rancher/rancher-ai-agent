0a3a60ff9e4b404b5517d9a493cdbf35e6264847
Raul Cabello <raulcabm@gmail.com>
2026-09-24 16:52:48 +0200

    plan integration tests

---
+++ tests/integration/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+
+@pytest.fixture(scope="module")
+def module_monkeypatch(request):
+    """
+    A module-scoped version of the monkeypatch fixture.
+    This fixture ensures that patches persist for the duration of the module,
+    and cleanup happens only once at the end of the module.
+    """
+    mpatch = MonkeyPatch()
+
+    yield mpatch
+
+    mpatch.undo()
